55b6e94a645298e056a3f87dcdc02f249d75c19e
Nitish Puri <npuri1903@gmail.com>
2023-01-08 19:44:18 +0530

    create image views.

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -156,6 +156,7 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain();
+		createImageViews();
 	}
 
 	void createSurface() {
@@ -448,6 +449,33 @@ private:
 		vkGetDeviceQueue(_device, indices.presentFamily.value(), 0, &_presentQueue);
 	}
 
+	void createImageViews() {
+		_swapChainImageViews.resize(_swapChainImages.size());
+
+		for (int i = 0; i < _swapChainImages.size(); ++i) {
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = _swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = _swapChainImageFormat;
+
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create image views");
+			}
+		}
+	}
+
 	bool checkValidationLayerSupport() {
 		uint32_t layerCount;
 		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
@@ -517,6 +545,10 @@ private:
 
 	void cleanup() {
 
+		for (auto imageView : _swapChainImageViews) {
+			vkDestroyImageView(_device, imageView, nullptr);
+		}
+
 		vkDestroySwapchainKHR(_device, _swapChain, nullptr);
 		vkDestroyDevice(_device, nullptr);
 
@@ -542,6 +574,7 @@ private:
 	std::vector<VkImage> _swapChainImages;
 	VkFormat _swapChainImageFormat;
 	VkExtent2D _swapChainExtent;
+	std::vector<VkImageView> _swapChainImageViews;
 	VkSurfaceKHR _surface;
 	VkDebugUtilsMessengerEXT _debugMessenger;
 };
