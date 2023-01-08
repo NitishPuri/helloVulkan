4d090aa0d0a6dd8bae57b795efe0c1c87fd073ff
Nitish Puri <npuri1903@gmail.com>
2023-01-08 19:26:07 +0530

    create swapchain.

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -10,6 +10,10 @@
 #include <set>
 #include <optional>
 
+#include <cstdlib>
+#include <limits>
+#include <algorithm>
+
 #include <iostream>
 
 
@@ -151,6 +155,7 @@ private:
 		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+		createSwapChain();
 	}
 
 	void createSurface() {
@@ -307,6 +312,89 @@ private:
 		return availableFormats[0];
 	}
 
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+
+		for (const auto& availablePresentMode : availablePresentModes) {
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+		if (capabilities.currentExtent.width != (std::numeric_limits<uint32_t>::max)() ) {
+			return capabilities.currentExtent;
+		}
+		else {
+			int width, height;
+			glfwGetFramebufferSize(_window, &width, &height);
+
+			VkExtent2D actualExtent = {
+				static_cast<uint32_t>(width), static_cast<uint32_t>(height)
+			};
+
+			actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width,
+				capabilities.maxImageExtent.width);
+			actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height,
+				capabilities.maxImageExtent.height);
+
+			return actualExtent;
+		}
+	}
+
+	void createSwapChain() {
+		SwapChainSupportDetails swapChainDetails = querySwapChainSupport(_physicalDevice);
+
+		VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainDetails.formats);
+		VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainDetails.presentModes);
+		VkExtent2D extent = chooseSwapExtent(swapChainDetails.capabilities);
+
+		uint32_t imageCount = swapChainDetails.capabilities.minImageCount + 1;
+		if (swapChainDetails.capabilities.maxImageCount > 0
+			&& imageCount > swapChainDetails.capabilities.maxImageCount) {
+			imageCount = swapChainDetails.capabilities.maxImageCount;
+		}
+
+		VkSwapchainCreateInfoKHR createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = _surface;
+
+		createInfo.minImageCount = imageCount;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.imageExtent = extent;
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+
+		QueueFamilyindices indices = findQueueFamilies(_physicalDevice);
+		uint32_t queueFamilyIndices[] = { indices.graphicsFamily.value(), indices.presentFamily.value() };
+
+		if (indices.graphicsFamily != indices.presentFamily) {
+			createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			createInfo.queueFamilyIndexCount = 2;
+			createInfo.pQueueFamilyIndices = queueFamilyIndices;
+		}
+		else {
+			createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			createInfo.queueFamilyIndexCount = 0;
+			createInfo.pQueueFamilyIndices = nullptr;
+		}
+
+		createInfo.preTransform = swapChainDetails.capabilities.currentTransform;
+		createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		createInfo.presentMode = presentMode;
+		createInfo.clipped = VK_TRUE;
+
+		createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+		if (vkCreateSwapchainKHR(_device, &createInfo, nullptr, &_swapChain) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create swap chain.");
+		}
+	}
+
 	void createLogicalDevice() {
 		QueueFamilyindices indices = findQueueFamilies(_physicalDevice);
 
@@ -421,6 +509,8 @@ private:
 	}
 
 	void cleanup() {
+
+		vkDestroySwapchainKHR(_device, _swapChain, nullptr);
 		vkDestroyDevice(_device, nullptr);
 
 		if (enableValidationLayers) {
@@ -441,6 +531,7 @@ private:
 	VkDevice _device;
 	VkQueue _graphicsQueue;
 	VkQueue _presentQueue;
+	VkSwapchainKHR _swapChain;
 	VkSurfaceKHR _surface;
 	VkDebugUtilsMessengerEXT _debugMessenger;
 };
